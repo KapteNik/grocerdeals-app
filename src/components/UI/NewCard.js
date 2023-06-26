c13f72f6b24192a9b173e6df523e0ec7aab4a448
Kapetanidis Nikos <n_kapetanidis@outlook.com>
2023-06-26 13:26:35 +0300

    Create ReviewStore component with NewCard component

--- src/components/UI/NewCard.js
+++ src/components/UI/NewCard.js
@@ -1,6 +1,39 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-const NewCard = (props) => {
+const manipulateLikes = (id, typeOfLike, setLoaded) => {
+  fetch(`http://localhost:3000/api/v1/offers/add${typeOfLike}`, {
+    method: "PATCH",
+    body: JSON.stringify({
+      id: id,
+      user: "642da89e984cea1f14a03a5a",
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    setLoaded((prevLoaded) => !prevLoaded);
+  });
+};
+
+// const manipulateStock = (id) => {
+//   fetch(`http://localhost:3000/api/v1/offers/increment${typeOfLike}`, {
+//     method: "PATCH",
+//     body: JSON.stringify({
+//       id: id,
+//       user: "642da89e984cea1f14a03a5a",
+//     }),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   }).then((res) => {
+//     setLoaded((prevLoaded) => !prevLoaded);
+//   });
+// };
+
+function NewCard(props) {
+  const [inStock, setInStock] = useState(null);
+
   return (
     <li className="card">
       <h3 className="upperLine">
@@ -9,25 +42,36 @@ const NewCard = (props) => {
       </h3>
       <h3 className="lowerLine">
         <div className="icons">
-          <button className="icon">
+          <button
+            className="icon"
+            onClick={() =>
+              manipulateLikes(props.id, "Dislike", props.setLoaded)
+            }
+          >
             {props.likes + " "}
             <i className="fa-solid fa-thumbs-up"></i>
           </button>
-          <button className="icon">
+          <button
+            className="icon"
+            onClick={() => manipulateLikes(props.id, "Like", props.setLoaded)}
+          >
             {props.dislikes + " "}
             <i className="fa-solid fa-thumbs-down"></i>
           </button>
-          <div className="icon">
+          <button
+            className="icon"
+            //  onClick={() => manipulateStock(props.id)}
+          >
             <i className="fa-solid fa-cart-shopping"></i>
-          </div>
-          <button className="icon">
-            <i className="fa-sharp fa-solid fa-circle-check"></i>
           </button>
+          <div className="icon">
+            <i className="fa-sharp fa-solid fa-circle-check"></i>
+          </div>
         </div>
         <div>24/1/22</div>
       </h3>
     </li>
   );
-};
+}
 
 export default NewCard;
--- src/pages/ReviewStore.js
+++ src/pages/ReviewStore.js
@@ -1,25 +1,53 @@
 import { useLocation } from "react-router-dom";
 import NewCard from "./../components/UI/NewCard";
+import { useParams } from "react-router-dom";
 import "./../components/UI/NewCard.css";
+import { useEffect, useState } from "react";
 
-const ProfilePage = () => {
-  const location = useLocation();
-  const offersArray = Object.values(location.state.offers);
-  console.log(location.state.offers);
-  console.log(location.state.offers[0]._id);
+function ProfilePage() {
+  // const location = useLocation();
+  // const offersArray = Object.values(location.state.offers);
+  const storeId = Object.values(useParams());
+  const [offers, setOffers] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+
+  // const toggleLoaded = () => {
+  //   setLoaded((prevLoaded) => !prevLoaded);
+  // };
+
+  useEffect(() => {
+    console.log(storeId);
+    fetch(`http://localhost:3000/api/v1/offers/getStoreOffers/${storeId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setOffers(data.data.offers);
+        // console.log(data.data.offers);
+      });
+  }, [loaded]);
+
+  if (!offers) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <ul>
-      {offersArray.map((offer) => (
+      {offers.map((offer) => (
         <NewCard
           key={offer._id}
+          id={offer._id}
           price={offer.price}
           likes={offer.likes}
           dislikes={offer.dislikes}
           name={offer.product.name}
+          setLoaded={setLoaded}
         />
       ))}
     </ul>
   );
-};
+}
 export default ProfilePage;
